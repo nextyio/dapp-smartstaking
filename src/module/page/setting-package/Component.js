1d1c805548d26483086dc020c434c99e6d32c97f
hoangnong <hoang.nong.it@gmail.com>
2018-06-15 21:21:04 +0700

    Remove code not use in setting package page

--- src/module/page/setting-package/Component.js
+++ src/module/page/setting-package/Component.js
@@ -11,89 +11,6 @@ const FormItem = Form.Item;
 
 export default class extends LoggedInPage {
 
-    setingPackage() {
-        let {contract} = this.props.profile
-        contract.setupPackage1(10)
-    }
-
-    toHex(str) {
-        var hex = '';
-
-        for(var i=0; i<str.length; i++) {
-            hex += '' + str.charCodeAt(i).toString(16);
-        }
-
-        return hex;
-    }
-
-    depositPackage1() {
-        const {contract, wallet, web3} = this.props.profile
-        const privatekey = wallet.getPrivateKey()
-
-        const nonce = web3.eth.getTransactionCount(wallet.getAddressString())
-
-        let data = '1'
-        const rawTx = {
-            nonce: nonce,
-            from: wallet.getAddressString(),
-            value: web3.toWei(0.1, "ether"),
-            to: contract.address,
-            data: '0x0000000000000000000000000000000000000000000000000000000000000001'
-        }
-
-        var gas = web3.eth.estimateGas(rawTx);
-        rawTx.gas = gas
-        // rawTx.data = '0x' + this.toHex(1)
-
-        console.log('rawTx', rawTx)
-        const tx = new Tx(rawTx)
-        tx.sign(privatekey)
-        const serializedTx = tx.serialize()
-
-        web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, hash) {
-            console.log('err', err)
-            if (!err)
-                console.log(hash)
-        })
-    }
-
-    renderContractInfo () {
-        let {contract} = this.props.profile
-        console.log('contract', contract)
-
-        if (!contract) {
-            return null
-        }
-
-        const fund = contract.fund()
-        const fundBonus = contract.fundBonus()
-        const package1 = contract.PACKAGE1()
-        const package2 = contract.PACKAGE2()
-        const package3 = contract.PACKAGE3()
-        const package4 = contract.PACKAGE4()
-        const MIN_AMOUNT_STAKING = contract.MIN_AMOUNT_STAKING()
-        const investors = contract.investors(0)
-        const packageCount = contract.getPackageCount()
-        const packageInfo = contract.getPackageInfo(0)
-        // contract.withdrawBonusPackage(0)
-        console.log('xxx', packageInfo.toString())
-
-        return (
-            <div>
-                <p>Total fund: {fund.toString()}</p>
-                <p>Total fund for bonus (interest): {fundBonus.toString()}</p>
-                <p>Count package: {packageCount.toString()}</p>
-                <p>Package Ids: {package1.toString()}, {package2.toString()}, {package3.toString()}, {package4.toString()}</p>
-                <Button type="ebp" htmlType="button" onClick={this.setingPackage.bind(this)} className="">
-                    Setting Package1
-                </Button>
-                <Button type="ebp" htmlType="button" onClick={this.depositPackage1.bind(this)} className="">
-                    Deposit Package
-                </Button>
-            </div>
-        )
-    }
-
     ord_renderContent () {
         let {wallet, web3, contract} = this.props.profile
         let balance
@@ -186,16 +103,13 @@ export default class extends LoggedInPage {
 
                     <Row style={{'marginTop': '15px'}}>
                         <Col span={4} offset={6}>
-                            
+
                         </Col>
                         <Col span={4}>
                             <Button onClick={this.reset.bind(this)} type="primary" className="btn-margin-top">Reset</Button>
                             <Button style={{'marginLeft': '15px'}} type="primary" onClick={this.set.bind(this)} className="btn-margin-top">Set</Button>
                         </Col>
                     </Row>
-                    {/* <p>Account Address: {address}</p> */}
-                    {/* <p>Account Balance: {balance}</p> */}
-                    {/* {this.renderContractInfo()} */}
                 </div>
             </div>
         )
@@ -211,11 +125,11 @@ export default class extends LoggedInPage {
     }
 
     set () {
-        
+        console.log('xxx', this.state)
     }
 
     reset () {
-        
+
     }
 
     onChange7Days(value) {
